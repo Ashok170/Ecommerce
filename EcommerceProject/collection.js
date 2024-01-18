ceed1f4e4b9d84d7856112ef2e344ae3a8ef16d5
Ashok170 <ashoklakshamanan@gmail.com>
2024-01-18 09:17:51 +0530

    going to update soon

---
+++ EcommerceProject/collection.js
@@ -0,0 +1,36 @@
+var productContainer = document.getElementById("products")
+var search = document.getElementById("search");
+var productList=productContainer.querySelectorAll("div");
+console.log(productList)
+search.addEventListener("keyup",function(event){
+    var enteredValue=event.target.value.toUpperCase();
+for(let i=0;i<productList.length;i++){
+    var productName=productList[i].querySelector("p").textContent;
+    if(productName.toUpperCase().indexOf(enteredValue)<0){
+        productList[i].style.display="none";
+    }else{
+        productList[i].style.display="block"
+    }
+}
+
+
+
+                                        })
+
+
+
+
+// var productContainer = document.getElementById("products")
+// var search = document.getElementById("search");
+// var productList = productContainer.querySelectorAll("div");
+// search.addEventListener("keyup", function () {
+//     var enteredValue = event.target.value.toUpperCase()
+//     for (let i = 0; i < productList.length; i++) {
+//         var productName = productList[i].querySelector("p").textContent
+//         if (productName.toUpperCase().indexOf(enteredValue) < 0) {
+//             productList[i].style.display = "none"
+//         }else{
+//             productList[i].style.display = "block"
+//         }
+//     }
+// })
---
+++ EcommerceProject/html.js
@@ -0,0 +1,6 @@
+function showsidenavbar(){
+    document.querySelector(".side-navbar").style.left="0";
+}
+function closesidenavbar(){
+    document.querySelector(".side-navbar").style.left="-60%";
+}
